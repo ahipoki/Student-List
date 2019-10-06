2f91b5fe31acc6690013897e1c1a385c4178c47d
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-05 20:01:24 -0700

    Create StudentList.cpp

---
+++ StudentList.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+//Method for adding students
+
+//Method for printing out students
+
+//Method for deleting students
+
+int main()
+{
+//First name character array
+//Last name character array
+//Student ID int
+//Decimal for GPA float
+//Vector pointer which points to a vector of struct pointers
+//Read in students
+//Print them out
+//Delete them
+}
