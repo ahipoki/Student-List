d386d5cc231794d7989bfba1093537ae65d6277f
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-13 20:16:01 -0700

    Update StudentList.cpp

--- StudentList.cpp
+++ StudentList.cpp
@@ -3,11 +3,32 @@
 
 using namespace std;
 
-//Method for adding students
+//Method for adding students {
+  //Create a new entry for a student
+  //After you type add {
+    //Prompt for first name
+    //Prompt for last name
+    //Prompt for student ID
+    //Prompt for GPA
+    //Add student to vector of students
+  //}
+//}
+//8:37
 
-//Method for printing out students
+//Method for printing out students {
+  //Print out all students currently stored
+  //Ex:
+  //Jason Galbraith, 487329, 5.00
+  //John Elstad, 903826, 3.25
+  //Kevin Jin, 468921, 0.12
+  //GPA shown to 2 decimal places
+//}
 
-//Method for deleting students
+//Method for deleting students {
+  //Prompt user for student ID number to delete
+  //Remove that struct from the vector
+  //Delete the data
+//}
 
 int main()
 {
@@ -19,4 +40,5 @@ int main()
 //Read in students
 //Print them out
 //Delete them
+//Finishes with a Quit command
 }
