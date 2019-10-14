6515d1957586fe0c71f8fb6fd2346b19e68e847f
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-10-13 20:34:51 -0700

    Update StudentList.cpp

--- StudentList.cpp
+++ StudentList.cpp
@@ -10,7 +10,8 @@ struc student {
   float gpa;
 };
 
-//Method for adding students {
+void add() {
+//Method for adding students
   //Create a new entry for a student
   //After you type add {
     //Prompt for first name
@@ -19,26 +20,43 @@ struc student {
     //Prompt for GPA
     //Add student to vector of students
   //}
-//}
+}
 //8:37
 
-//Method for printing out students {
+void print() {
+//Method for printing out students
   //Print out all students currently stored
   //Ex:
   //Jason Galbraith, 487329, 5.00
   //John Elstad, 903826, 3.25
   //Kevin Jin, 468921, 0.12
   //GPA shown to 2 decimal places
-//}
+}
 
-//Method for deleting students {
+void delete() {
+//Method for deleting students
   //Prompt user for student ID number to delete
   //Remove that struct from the vector
   //Delete the data
-//}
+}
 
 int main()
 {
+  char input = 0;
+  cout << "Do you want to add, print, or delete a student? (A/P/D)" << endl;
+  cin >> input;
+  if (input == 'A') {
+    add();
+  }
+  else if (input == 'P') {
+    print();
+  }
+  else if (input == 'D') {
+    delete();
+  }
+  else {
+    cout << "That's an invalid input. Please enter either A, P, or D" << endl;
+  }
 //Vector pointer which points to a vector of struct pointers
 //Read in students
 //Print them out
