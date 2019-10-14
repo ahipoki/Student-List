e53e374cfb952b267f2b4500805f3ba3c7d4477a
Your Name <ahep0ki@gmail.com>
2019-10-14 10:30:11 -0700

    Update 1

--- StudentList.cpp
+++ StudentList.cpp
@@ -3,27 +3,31 @@
 
 using namespace std;
 
-struc student {
-  char firstName[];
-  char lastName[];
+struct Student {
+  char firstName[10];
+  char lastName[10];
   int id;
   float gpa;
 };
 
 void add() {//Method for adding students
   //Create a new entry for a student
-  //After you type add {
-    cout << "Enter a first name" << endl;
-    cout << "Enter a last name" << endl;
-    cout << "Enter a student ID" << endl;
-    cout << "Enter a GPA" << endl;
-    //Add student to vector of students
-  //}
+  Student add;
+  //After you type add
+  cout << "Enter a first name" << endl;
+  cin >> add.firstName;
+  cout << "Enter a last name" << endl;
+  cin >> add.lastName;
+  cout << "Enter a student ID" << endl;
+  cin >> add.id;
+  cout << "Enter a GPA" << endl;
+  cin >> add.gpa;
+  cout << add.firstName << "Last name:" << add.lastName << " ID: " << add.id << " GPA:";
+  cout << add.gpa << endl;
+  //Add student to vector of students
 }
-//8:37
 
-void print() {
-//Method for printing out students
+void print() {//Method for printing out students
   //Print out all students currently stored
   //Ex:
   //Jason Galbraith, 487329, 5.00
@@ -32,8 +36,7 @@ void print() {
   //GPA shown to 2 decimal places
 }
 
-void delete() {
-//Method for deleting students
+void remove() {//Method for deleting students
   cout << "Enter the student ID you wish to delete" << endl;
   //Remove that struct from the vector
   //Delete the data
@@ -42,20 +45,33 @@ void delete() {
 int main()
 {
   char input = 0;
-  cout << "Do you want to add, print, or delete a student? (Add/Print/Delete)" << endl;
-  cin >> input;
-  if (input == 'A') {
-    add();
-  }
-  else if (input == 'P') {
-    print();
-  }
-  else if (input == 'D') {
-    delete();
-  }
-  else {
-    cout << "That's an invalid input. Please enter either A, P, or D" << endl;
-  }
+  //cout << "Do you want to add, print, or delete a student? (Add/Print/Delete)" << endl;
+  //cin >> input;
+  //if (input. == "Add") {
+  //add();
+  //}
+  Student test;
+  cout << "Enter a name" << endl;
+  cin >> test.firstName;
+  cout << "Enter a last name" << endl;
+  cin >> test.lastName;
+  cout << "Enter a student ID" << endl;
+  cin >> test.id;
+  cout << "Enter a GPA" << endl;
+  cin >> test.gpa;
+  cout << "First name: " << test.firstName << endl;
+  cout << "Last name: " << test.lastName << endl;
+  cout << "ID: " << test.id << endl;
+  cout << "GPA: " << test.gpa << endl;
+    //else if (input == "Print") {
+    //print();
+    //}
+    //else if (input == "Delete") {
+    //remove();
+    //}
+    //else {
+    //cout << "That's an invalid input. Please enter either A, P, or D" << endl;
+    //}
 //Vector pointer which points to a vector of struct pointers
 //Read in students
 //Print them out
