4dadf46031ddd12a2b3ddc1ef6a737064aee719a
Your Name <ahep0ki@gmail.com>
2019-10-24 10:26:17 -0700

    5th Commit

--- StudentList.cpp
+++ StudentList.cpp
@@ -12,83 +12,49 @@ struct Student {
   float gpa;
 };
 
-void addStudent(vector<Student> &vecS) {//Method for adding students
+void addStudent(vector<Student*> *vecS) {//Method for adding students
   cout << "Add" << endl;
-  //Create a new entry for a student
-  //struct Student firstAdd = { 0 };
-  //char firstAdd[10];
-  //char lastAdd[10];
-  //int id = 0;
-  //float gpa = 0.0;
-  //Student add;
-  //vector<Student> sub;
-  //std::vector<Student*> vecA;
-  //Student* sA = new Student;
-  //After you type add
-  //vecA.push_back(Student());
-  //vecS.push_back(Student());
+  //Create a new student
+  Student* s = new Student;
+  //Push into that student
+  //Push that into the vector
   cout << "Enter a first name: " << endl;
-  cin >> vecS.firstName;
-  //Student[0].firstName = "John";
-  //firstAdd = &nameAdd;
+  cin >> s->firstName;
   cout << "Enter a last name: " << endl;
-  //cin >> lastAdd;
-  //vecS.push_back(Student());
-  //vecS[1].lastName = "Smith";
-  //sA->lastName;
+  cin >> s->lastName;
   cout << "Enter a student ID: " << endl;
-  //cin >> id;
-  //sA->id = add.id;
+  cin >> s->id;
   cout << "Enter a GPA: " << endl;
-  //cin >> gpa;
-  //sA->gpa = add.gpa;
-  //vecA.push_back(sA);
-  cout << "First name: " << firstAdd << " Last name: " << lastAdd << " ID: " << id << " GPA: " << gpa << endl;
+  cin >> s->gpa;
+  //cout << "First name: " << *s->firstName << " Last name: " << *s->lastName << " ID: " << s->id << " GPA: " << s->gpa << endl;
   //Add student to vector of students
 }
 
-void printStudent(vector<Student> &vecS) {//Method for printing out students
-  cout << "Print" << endl;
-  //vector<Student*> vecP;
-  //std::vector<Student*>::iterator it = vecP.begin();
-  //for (vector<Student*>::iterator it = vecS.begin(); it != vecS.end(); it++) {
-  //cout << *it << ' ';
-  //}
-  //cout << "Test" << endl;
-  //cout << "Students: ";
-  //for (ptr = vecP.begin(); ptr < vecP.end(); ptr++) {
-    //cout << *ptr << " ";
-  //}
-
-  //Print out all students currently stored
-  //Ex:
-  //Jason Galbraith, 487329, 5.00
-  //John Elstad, 903826, 3.25
-  //Kevin Jin, 468921, 0.12
-  //GPA shown to 2 decimal places
+void printStudent(vector<Student*> *vecS) {//Method for printing out students
+  for (vector<Student*>::iterator i = vecS->begin(); i != vecS->end(); i++) {
+    cout << (*i)->firstName << ' ' << (*i)->lastName << ' ' << (*i)->id << ' ' << (*i)->gpa << ' ' << endl;
+  }
 }
 
-void removeStudent(vector<Student> &vecS) {//Method for deleting students
-  cout << "Delete" << endl;
-  //std::vector<Student> del;
-  //del.push_back('John');
-  //del.push_back('Smith');
-  //del.push_back(2);
-  //int id = 0;
-  //cout << "Enter the student ID you wish to delete" << endl;
-  //cin >> id;
-  //vecS.begin() + (id - 1);
-  //vecS.erase(vecS.begin() + id);
-  //cout << "ID: " << id << endl;
-  //Remove that struct from the vector
-  //Delete the data
+void removeStudent(vector<Student*> *vecS) {//Method for deleting students
+  int IdInput = 0;
+  cout << "Enter the ID of the student you want to remove" << endl;
+  cin >> IdInput;
+  cout << "ID: " << IdInput << endl;
+  for (vector<Student*>::iterator i = vecS->begin(); i != vecS->end(); i++) {
+    if (IdInput == (*i)->id) {
+      delete *i;
+      vecS->erase(i);
+      return;
+    }
+  }
 }
 
 int main()
 {
   bool stillUsing = true;
   char action = 0;
-  std::vector<Student> vecS;
+  std::vector<Student*> *vecS;
   std::vector<Student*> vecP;
 
   Student* sP = new Student;
@@ -101,27 +67,18 @@ int main()
     cin.getline(input, sizeof(input));
     if (strcmp(input, "ADD") == 0) {
       addStudent(vecS);
-      //vecS.push_back(s1);
-      //cout << sP->firstName << " " << sP->lastName;
     }
     else if (strcmp(input, "PRINT") == 0) {
       printStudent(vecS);
-      //vecS.push_back(s1);
-      //cout << sP->firstName << " " << sP->lastName;
     }
     else if (strcmp(input, "DELETE") == 0) {
       removeStudent(vecS);
-      //vecS.push_back(s1);
-      //cout << sP->firstName << " " << sP->lastName;
     }
     else if (strcmp(input, "QUIT") == 0) {
       stillUsing = false;
-      //vecS.push_back(s1);
-      //cout << sP->firstName << " " << sP->lastName;
     }
     else {
       cout << "That is an invalid option" << endl;
-      //vecS.push_back(s1);
     }
   }
 }
