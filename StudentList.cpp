500f25f248be8490496389dcdf591968c8f8b368
Your Name <ahep0ki@gmail.com>
2019-10-28 10:49:06 -0700

    Commit

--- StudentList.cpp
+++ StudentList.cpp
@@ -5,80 +5,109 @@
 
 using namespace std;
 
-struct Student {
+struct Student {//Struct of students
   char firstName[10];
+  //First name
   char lastName[10];
+  //Last name
   int id;
+  //ID
   float gpa;
+  //GPA
 };
 
-void addStudent(vector<Student*> *vecS) {//Method for adding students
-  cout << "Add" << endl;
+void addStudent(vector<Student*>*vecS) {//Method for adding students
   vecS->push_back(new Student());
-  cin.getline(vecS->at(vecS->size()-1)->firstName, 80);
-  cin.getline(vecS->at(vecS->size()-1)->lastName, 80);
-  cin.getline(vecS->at(vecS->size()-1)->id, 80);
-  cin.getline(vecS->at(vecS->size()-1)->gpa, 80);
-  //Create a new student
-  Student* s = new Student;
-  //Push into that student
-  //Push that into the vector
+  //Push back a new student
   cout << "Enter a first name: " << endl;
-  cin >> vecS->firstName;
+  //Prompt for a first name
+  cin.getline(vecS->at(vecS->size()-1)->firstName, 10);
+  //Get the input and add it to the vector
   cout << "Enter a last name: " << endl;
-  cin >> vecS->lastName;
-  cout << "Enter a student ID: " << endl;
-  cin >> vecS->id;
-  cout << "Enter a GPA: " << endl;
-  cin >> vecS->gpa;
-  //cout << "First name: " << *s->firstName << " Last name: " << *s->lastName << " ID: " << s->id << " GPA: " << s->gpa << endl;
-  //Add student to vector of students
+  //Prompt for a last name
+  cin.getline(vecS->at(vecS->size()-1)->lastName, 10);
+  //Get the input and add it to the vector
+  do {
+    cout << "Enter a student ID: " << endl;
+    //Prompt for an ID
+    cin >> vecS->at(vecS->size()-1)->id;
+    cin.clear();
+    cin.ignore(1000, '\n');
+  }while(cin.fail());
+  //Get the input and add it to the vector
+  do {
+    cout << "Enter a GPA: " << endl;
+    //Prompt for a GPA
+    cin >> vecS->at(vecS->size()-1)->gpa;
+    cin.clear();
+    cin.ignore(1000, '\n');
+  }while(cin.fail());
+  //Get the input and add it to the vector
 }
 
 void printStudent(vector<Student*> *vecS) {//Method for printing out students
-  for (vector<Student*>::iterator i = vecS->begin(); i != vecS->end(); i++) {
-    cout << (*i)->firstName << " " << (*i)->lastName << " " << (*i)->id << " " << (*i)->gpa << " " << endl;
+  for (vector<Student*>::iterator i = vecS->begin(); i != vecS->end(); i++) {//For the vector of students, use an iterator to go from the beginning to the end
+    cout << (*i)->firstName << " " << (*i)->lastName << ", " << (*i)->id << ", " <<((float)(int)(((*i)->gpa)*100))/100 << endl;
+    //Print out the first and last names, id, and gpa
   }
 }
 
-void removeStudent(vector<Student*> *vecS) {//Method for deleting students
+void deleteStudent(vector<Student*> *vecS) {//Method for deleting students
   int IdInput = 0;
   cout << "Enter the ID of the student you want to remove" << endl;
+  //Prompt for the ID of the student to delete
   cin >> IdInput;
-  for (vector<Student*>::iterator i = vecS->begin(); i != vecS->end(); i++) {
-    if (IdInput == (*i)->id) {
+  //Take in that input
+  for (vector<Student*>::iterator i = vecS->begin(); i != vecS->end(); i++) {//For the vector of students, use an iterator and go through it
+    if (IdInput == (*i)->id) {//If the input is in the vector
       delete *i;
+      //Delete the student
       vecS->erase(i);
+      //Erase the data
       return;
+      //Return
+    }
+    else if (IdInput != (*i)->id) {//If the input is not in the vector
+      cout << "That ID is not in the list of students" << endl;
+      //Tell the user the input is not in the vector
     }
   }
 }
 
 int main()
 {
-  char action = 0;
-  vector<Student*> *vecS;
+  vector<Student*> *vecS = new vector<Student*>;
+  //A vector of students
 
-  Student* sP = new Student;
-  
-  while (true) {
+  while (true) {//While the quit function hasn't been used
     char input[80];
     cout << "Do you want to add a student, print out the students, delete a student, or quit? (ADD/PRINT/DELETE/QUIT)" << endl;
+    //Ask if they want to add, print, delete or quit
     cin.getline(input, sizeof(input));
-    if (strcmp(input, "ADD") == 0) {
+    //Get their input
+    if (strcmp(input, "ADD") == 0) {//If they say ADD
       addStudent(vecS);
+      //Call the add method
+      cin.ignore(1000, '\n');
+      //Prevent printing multiple lines
     }
-    else if (strcmp(input, "PRINT") == 0) {
+    else if (strcmp(input, "PRINT") == 0) {//Else if they say PRINT
       printStudent(vecS);
+      //Call the print method
     }
-    else if (strcmp(input, "DELETE") == 0) {
-      removeStudent(vecS);
+    else if (strcmp(input, "DELETE") == 0) {//Else if they say DELETE
+      deleteStudent(vecS);
+      //Call the delete method
+      cin.ignore(1000, '\n');
+      //Prevent printing multiple lines
     }
-    else if (strcmp(input, "QUIT") == 0) {
+    else if (strcmp(input, "QUIT") == 0) {//Else if they say QUIT
       return false;
+      //End the program
     }
-    else {
-      cout << "That is an invalid option" << endl;
+    else {//Else if they say anything else
+      cout << "That is an invalid option." << endl;
+      //Tell them that's an invalid option
     }
   }
 }
